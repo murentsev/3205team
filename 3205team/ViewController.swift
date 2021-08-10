f04235e7b9ea43e34cee3557d6a338124418546e
Alexey Murentsev <murentsev@icloud.com>
2021-08-10 07:40:21 +0300

    Initial Commit

---
+++ 3205team/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3205team
+//
+//  Created by Алексей Муренцев on 10.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
